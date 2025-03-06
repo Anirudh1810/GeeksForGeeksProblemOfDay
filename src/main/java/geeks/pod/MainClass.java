e34f42b7923423fe0ca81b079503be75d8a9fbf4
anirudh1810 <Sandy@!332715>
2025-03-06 11:11:31 +0530

    march 6 problem solution

---
+++ src/main/java/geeks/pod/MainClass.java
@@ -0,0 +1,7 @@
+package geeks.pod;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println(March062025.lcs("ABCDGH", "AEDFHR"));
+    }
+}
--- src/main/java/geeks/pod/March062025.java
+++ src/main/java/geeks/pod/March062025.java
@@ -1,22 +1,19 @@
 package geeks.pod;
 
 public class March062025 {
-    public static void main(String[] args) {
-    System.out.println(lcs("ABCDGH", "AEDFHR"));
-    }
 
     static int lcs(String s1, String s2) {
         // code here
-        int m=s1.length();
-        int n=s2.length();
+        int m = s1.length();
+        int n = s2.length();
 
-        int[][] dp=new int[m+1][n+1];
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]+1;
-                }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
